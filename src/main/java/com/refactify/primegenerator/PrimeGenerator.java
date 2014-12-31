b16a2ac558beb478a687f0228db289d47500f35f
Daniel Aquino <daniel@egovtn.com>
2014-12-30 22:24:38 -0600

    Final reread. Renamed calculateMaxPrimeFactor to determineIterationLimit. Added new exhaustive unit test to verify prime number generation from 2 to 500. The book removed the  + 1 part from calculating the iteration limit.  However, the exhaustive unit test fails with Java 6+.  So, I had to keep the + 1 part in calculating the iteration limit.

--- src/main/java/com/refactify/primegenerator/PrimeGenerator.java
+++ src/main/java/com/refactify/primegenerator/PrimeGenerator.java
@@ -2,7 +2,7 @@ package com.refactify.primegenerator;
 
 public class PrimeGenerator {
 
-    private static boolean[] isCrossed;
+    private static boolean[] crossedOut;
     private static int[] result;
 
 
@@ -11,45 +11,46 @@ public class PrimeGenerator {
             return new int[0];
         }
         else {
-            initializeArrayOfIntegers(maxValue);
+            uncrossIntegersUpTo(maxValue);
             crossOutMultiples();
             putUncrossedIntegersIntoResult();
             return result;
         }
     }
 
-    private static void initializeArrayOfIntegers(final int maxValue) {
-        isCrossed = new boolean[maxValue + 1];
-        for (int i = 2; i < isCrossed.length; i++)
-            isCrossed[i] = false;
+    private static void uncrossIntegersUpTo(final int maxValue) {
+        crossedOut = new boolean[maxValue + 1];
+        for (int i = 2; i < crossedOut.length; i++)
+            crossedOut[i] = false;
     }
 
     private static void crossOutMultiples() {
-        int maxPrimeFactor = calculateMaxPrimeFactor();
-        for (int i = 2; i < maxPrimeFactor; i++) {
+        int limit = determineIterationLimit();
+        for (int i = 2; i < limit; i++) {
             if(notCrossed(i)) {
                 crossOutMultiplesOf(i);
             }
         }
     }
 
-    private static int calculateMaxPrimeFactor() {
-        return (int)Math.sqrt(isCrossed.length) + 1;
+    private static int determineIterationLimit() {
+        double iterationLimit = Math.sqrt(crossedOut.length) + 1;
+        return (int)iterationLimit;
     }
 
     private static boolean notCrossed(final int i) {
-        return isCrossed[i] == false;
+        return crossedOut[i] == false;
     }
 
     private static void crossOutMultiplesOf(final int i) {
-        for (int multiple = 2 * i; multiple < isCrossed.length; multiple += i) {
-            isCrossed[multiple] = true;
+        for (int multiple = 2 * i; multiple < crossedOut.length; multiple += i) {
+            crossedOut[multiple] = true;
         }
     }
 
     private static void putUncrossedIntegersIntoResult() {
         result = new int[numberOfUncrossedIntegers()];
-        for (int i = 2, j = 0; i < isCrossed.length; i++) {
+        for (int i = 2, j = 0; i < crossedOut.length; i++) {
             if(notCrossed(i)) {
                 result[j++] = i;
             }
@@ -58,7 +59,7 @@ public class PrimeGenerator {
 
     private static int numberOfUncrossedIntegers() {
         int count = 0;
-        for (int i = 2; i < isCrossed.length; i++) {
+        for (int i = 2; i < crossedOut.length; i++) {
             if(notCrossed(i))
                 count++;
         }
