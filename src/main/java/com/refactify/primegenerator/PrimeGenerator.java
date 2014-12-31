e31ce6e4827306d0b72acf8bbe193e678d3a795f
Daniel Aquino <daniel@egovtn.com>
2014-12-30 21:58:40 -0600

    Removed usage of s variable by setting the length of the f array and using f.length in the place of s. Renamed the three methods to have more expressive names. Re-arranged the initializeArrayOfIntegers method to read more nicely. Renamed primes array to result.

--- src/main/java/com/refactify/primegenerator/PrimeGenerator.java
+++ src/main/java/com/refactify/primegenerator/PrimeGenerator.java
@@ -2,9 +2,8 @@ package com.refactify.primegenerator;
 
 public class PrimeGenerator {
 
-    private static int s;
     private static boolean[] f;
-    private static int[] primes;
+    private static int[] result;
 
 
     public static int[] generatePrimes(final int maxValue) {
@@ -12,51 +11,46 @@ public class PrimeGenerator {
             return new int[0];
         }
         else {
-            initializeSieve(maxValue);
-            sieve();
-            loadPrimes();
-            return primes;
+            initializeArrayOfIntegers(maxValue);
+            crossOutMultiples();
+            putUncrossedIntegersIntoResult();
+            return result;
         }
     }
 
-    private static void initializeSieve(final int maxValue) {
-        s = maxValue + 1;
-        f = new boolean[s];
-        int i;
-
-        for (i = 0; i < s; i++) {
-            f[i] = true;
-        }
-
+    private static void initializeArrayOfIntegers(final int maxValue) {
+        f = new boolean[maxValue + 1];
         f[0] = f[1] = false;
+        for (int i = 2; i < f.length; i++)
+            f[i] = true;
     }
 
-    private static void sieve() {
+    private static void crossOutMultiples() {
         int i;
         int j;
-        for (i = 2; i < Math.sqrt(s) + 1; i++) {
+        for (i = 2; i < Math.sqrt(f.length) + 1; i++) {
             if(f[i]) {
-                for (j = 2 * i; j < s; j += i) {
+                for (j = 2 * i; j < f.length; j += i) {
                     f[j] = false;
                 }
             }
         }
     }
 
-    private static void loadPrimes() {
+    private static void putUncrossedIntegersIntoResult() {
         int i;
         int j;
 
         int count = 0;
-        for (i = 0; i < s; i++) {
+        for (i = 0; i < f.length; i++) {
             if(f[i])
                 count++;
         }
 
-        primes = new int[count];
-        for (i = 0, j = 0; i < s; i++) {
+        result = new int[count];
+        for (i = 0, j = 0; i < f.length; i++) {
             if(f[i]) {
-                primes[j++] = i;
+                result[j++] = i;
             }
         }
     }
