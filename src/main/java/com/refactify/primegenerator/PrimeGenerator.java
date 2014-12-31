f924f9178ea73bb6157517d0643ebd7886181cc7
Daniel Aquino <daniel@egovtn.com>
2014-12-30 21:52:05 -0600

    This is the first set of refactorings. All we did was break the generatePrimes function into three smaller functions and promote some variables to static fields of the class.

---
+++ src/main/java/com/refactify/primegenerator/PrimeGenerator.java
@@ -0,0 +1,63 @@
+package com.refactify.primegenerator;
+
+public class PrimeGenerator {
+
+    private static int s;
+    private static boolean[] f;
+    private static int[] primes;
+
+
+    public static int[] generatePrimes(final int maxValue) {
+        if(maxValue < 2) {
+            return new int[0];
+        }
+        else {
+            initializeSieve(maxValue);
+            sieve();
+            loadPrimes();
+            return primes;
+        }
+    }
+
+    private static void initializeSieve(final int maxValue) {
+        s = maxValue + 1;
+        f = new boolean[s];
+        int i;
+
+        for (i = 0; i < s; i++) {
+            f[i] = true;
+        }
+
+        f[0] = f[1] = false;
+    }
+
+    private static void sieve() {
+        int i;
+        int j;
+        for (i = 2; i < Math.sqrt(s) + 1; i++) {
+            if(f[i]) {
+                for (j = 2 * i; j < s; j += i) {
+                    f[j] = false;
+                }
+            }
+        }
+    }
+
+    private static void loadPrimes() {
+        int i;
+        int j;
+
+        int count = 0;
+        for (i = 0; i < s; i++) {
+            if(f[i])
+                count++;
+        }
+
+        primes = new int[count];
+        for (i = 0, j = 0; i < s; i++) {
+            if(f[i]) {
+                primes[j++] = i;
+            }
+        }
+    }
+}
